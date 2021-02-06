6800707842c8c07b64d7eacbdf140bd57fc2fed1
juliosimply <julio_simply@hotmail.com>
2021-02-06 11:15:56 -0300

    Exercicios 001 a 007

---
+++ Aula_008.py
@@ -0,0 +1,26 @@
+# As primeiras aulas foram compatadas juntas
+# aula 001 -> apresentaçao do curso
+# aula 002 -> para que serve o python
+# aula 003 -> instalaçao python
+# aula 004 -> primeiros comandos com idle
+# aula 005 -> instalaçao pycharm e qpython
+# aula 006 -> tipos primitivos (int, float, str, bool(True e False case sensitive T,F))
+# aula 007 -> operadores (+, -, *, /, % (resto da divisao), ** (potencia), // (divisao inteira)
+# ordem de precedencia (), **, *,/, //, %, +,-
+
+#-> TIPOS PRIMITIVOS
+import math
+n1 = int (input('digite um valor'))
+n2 = int (input('Digite outro'))
+s = n1 + n2
+print ('a soma vale', s)
+print(' a soma entre {0} e {1} vale {1}'.format(n1, n2,s))
+print(n1
+n1 = int.isnumweric
+)
+# raiz quadrada 81 = 81**(1/2) cubica 127**(1/3) potencia = pow(4,3)
+
+
+
+
+
---
+++ Exercicio001-007.py
@@ -0,0 +1,46 @@
+#Exercício 001
+#print('olá mundo!')
+
+#Exercicio 002
+#nome = input("qual o seu nome?")
+#print(' Muito prazer', nome, 'seja muito Bem vindo')
+
+#Exercicio 003
+
+# n1 = int (input('Digite un número:'))
+# n2 = int  (input('Digite outro numero:'))
+#s = n1 + n2
+#print('a soma de {} + {}  é {}'.format(n1, n2, s))
+
+#Exercício 004
+#a = input('Digite algo:')
+#print('o tipo primitivo desse valor é', type(a))
+#print(' Só tem espaços?', a.isspace())
+#print("é um numero?",a.isnumeric())
+#print('é alfabetico?', a.isalpha())
+#print('é alfanumerico?', a.isalnum())
+#print('esta em maiusculas?', a.isupper())
+#print('Esta em minusculas?', a.islower())
+#print('ela esta capitalizada?', a.istitle())
+
+#Exercicio 005
+
+#n = int (input('Digite um numero'))
+# antecessor =  n-1
+#sucessor = n+1  (ou pode ser feito tudo apenas com uma variavel)
+#print('o numero é {}, o antecessor é {}, o sucessor é {}'.format(n, (n-1), (n+1)))
+
+#Exercicio 006
+
+# n = int (input('Digite un numero'))
+# nq = n * 2
+# t = n * 3
+# rq = n ** (1/2)
+# print('o dobro de {}, é {}, o tripl é {}, a raiz quadrada é {:.2f}'.format(n, nq,t, rq)) #funçao raiz quadrada (pow (n, (1/2)
+
+# Exercicio 007
+
+nt1 = float (input('Digite nota 1?'))
+nt2 = float (input('Digite nota 2'))
+media = (nt1 + nt2)/2
+print('a sua média é {:.2f}'.format(media))
