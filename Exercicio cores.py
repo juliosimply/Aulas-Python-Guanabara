1da2f6e535ff95b848a9e4ef71b386d27a8fe47a
juliosimply <julio_simply@hotmail.com>
2021-02-08 17:26:44 -0300

    Codigo de cores no pycharm

---
+++ Exercicio cores.py
@@ -0,0 +1,4 @@
+print("\033[7;33;44m olá, mundo!\033[m")
+'''style = 0 =none, 1 = negrito, 4 = sublinhado, 7 negativo
+text = 30 = branco, 31 = vermelho, 32 = verde, 33 = amarelo, 34 = azul, 35 = lilas, 36 = azul claro, 37 = a cinza
+background = text com as dezenas 40, 41...'''
