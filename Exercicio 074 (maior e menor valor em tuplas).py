ab05c1bf2e86989b5b220a1517eef04131549296
juliosimply <julio_simply@hotmail.com>
2021-02-20 16:54:48 -0300

    Exercicio 074 (sorteio com tuplas)

---
+++ Exercicio 074 (maior e menor valor em tuplas).py
@@ -0,0 +1,5 @@
+from random import randint
+sorteio = (randint(1,10),randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+print( f'os numeros sorteados foram {sorteio}')
+print(f' o maior numero foi {max(sorteio)}')
+print(f'o menor numero foi {min(sorteio)}')
