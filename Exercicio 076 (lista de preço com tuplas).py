3b440498c83e125e25599b3f72b9da2de9566349
juliosimply <julio_simply@hotmail.com>
2021-02-21 08:17:51 -0300

    Exercicio 076 (lista de preço com tuplas)

---
+++ Exercicio 076 (lista de preço com tuplas).py
@@ -0,0 +1,17 @@
+listagem = ('Lápis', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.90,
+            'Estojo', 25,
+            'Transferidor', 4.20,
+            'Compasso', 9.99,
+            'Mochila', 120.99,
+            'Canetas', 5.99,
+            'Livro', 89.99)
+print('-' * 30)
+print('LISTAGEM DE PREÇOS')
+print('-' * 30)
+for pos in range (0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:.>7.2f}')
