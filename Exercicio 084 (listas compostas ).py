27ac70f20ca79acf6405515ac98e304180b66ee5
juliosimply <julio_simply@hotmail.com>
2021-02-27 10:59:02 -0300

    Exercicio 084 (listas compostas)

--- Exercicio 084 (listas compostas ).py
+++ Exercicio 084 (listas compostas ).py
@@ -1,24 +1,28 @@
-nome =list()
-peso = list()
+nome = []
 total = []
-maior=menor=0
+maior = menor = 0
 while True:
     nome.append(str(input('Nome:')))
-    peso.append(int(input('Peso:')))
-    total.append(peso)
+    nome.append(float(input('Peso:')))
+    if len(total) == 0:
+        maior = menor = nome[1]
+    else:
+        if nome[1] > maior:
+            maior = nome[1]
+        if nome[1] < menor:
+            menor = nome[1]
+    total.append(nome[:])
+    nome.clear()
     resp = str(input('Quer comtinuar [S/N]?'))
     if resp in 'nN':
         break
-    if len(total) == 0:
-        peso = menor = maior = 0
-    else:
-        if total[0] < menor:
-            menor = total[0]
-        if total[0] > maior:
-            maior = total[0]
+
 print(f'o mais pesado foi {maior}')
+for p in total:
+    if p[1] == maior:
+        print(f'{p[0]}')
 print(f'o mais leve foi {menor}')
-print('o total de cadastrados foi',len(nome))
+print('o total de cadastrados foi',len(total))
 
 
 
